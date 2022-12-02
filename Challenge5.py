91c267aae04fc0f89a9dfdd11f91093dcc36a79b
BetsyC SandovalP <84147734+CarolinaDevSP@users.noreply.github.com>
2022-12-02 00:17:04 -0500

    Delete Challenge5.py

--- Challenge5.py
+++
@@ -1,5 +0,0 @@
-#graficar estudiantes y sus promedios
-#extraer sus olumnas y pasarlos como parametros para graficar
-
-#leer doc de pandas y matlolip
-#calcular el promedio de todos los estudiantes usando la columna de final
